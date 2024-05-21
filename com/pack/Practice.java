6bc6af798aa7f0fc27a29cf31b693bd72c740799
Lavanya <lavanya957347@gmail.com>
2024-05-21 10:27:56 +0530

    prime added

--- com/pack/Practice.java
+++ com/pack/Practice.java
@@ -3,7 +3,7 @@ package com.pack;
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to java programming language");
 
 	}
 
